8c3225091afd8f4cde8c73cb3726faed739bd8b8
meer-khan <shahmirkhan519@gmail.com>
2024-10-27 06:10:10 +0500

    basics

--- src/basics.js
+++ src/basics.js
@@ -1,1 +1,30 @@
 console.log("Hellow World!")
+
+
+console.log(Array())
+
+const b = new Boolean(false);
+console.log(`B is : ${b}`)
+console.log(b)
+
+if (b) {
+  // this condition evaluates to true
+  console.log(`first if  : ${b}`)
+}
+if (b == false) {
+  // this condition evaluates to false
+  console.log(b)
+}
+
+
+
+fruitType = "Cherry"
+
+switch (fruitType) {
+    case "Cherry": 
+        console.log("Hellow 1")
+
+    case "Cherry": 
+    console.log("Hellow 2")
+    
+}
